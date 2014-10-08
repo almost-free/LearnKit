4b04ab208d43cdd43eac2684d0246e4f7d0dc77b
Matt Rajca <matt.rajca@me.com>
2014-10-08 15:29:36 -0500

    Added supplementary files

---
+++ LearnKit/LearnKit.h
@@ -0,0 +1,16 @@
+//
+//  LearnKit.h
+//  LearnKit
+//
+//  Copyright (c) 2014 Matt Rajca. All rights reserved.
+//
+
+#import <LearnKit/LNKTypes.h>
+
+#import <LearnKit/LNKDesignMatrix.h>
+#import <LearnKit/LNKLinRegPredictor.h>
+#import <LearnKit/LNKLogRegClassifier.h>
+#import <LearnKit/LNKNeuralNetClassifier.h>
+#import <LearnKit/LNKOneVsAllLogRegClassifier.h>
+#import <LearnKit/LNKOptimizationAlgorithm.h>
+#import <LearnKit/LNKUtilities.h>
