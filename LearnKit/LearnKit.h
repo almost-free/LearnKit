37097e7834545f508a0f76e11a8313ffd86c0d96
Matt Rajca <matt.rajca@me.com>
2014-10-08 17:45:23 -0500

    Updated LearnKit.h

--- LearnKit/LearnKit.h
+++ LearnKit/LearnKit.h
@@ -7,10 +7,17 @@
 
 #import <LearnKit/LNKTypes.h>
 
-#import <LearnKit/LNKMatrix.h>
+#import <LearnKit/LNKClassifier.h>
+#import <LearnKit/LNKKMeansClassifier.h>
+#import <LearnKit/LNKKNNClassifier.h>
 #import <LearnKit/LNKLinRegPredictor.h>
 #import <LearnKit/LNKLogRegClassifier.h>
+#import <LearnKit/LNKMatrix.h>
+#import <LearnKit/LNKMatrixPCA.h>
+#import <LearnKit/LNKMatrixUI.h>
+#import <LearnKit/LNKNaiveBayesClassifier.h>
 #import <LearnKit/LNKNeuralNetClassifier.h>
 #import <LearnKit/LNKOneVsAllLogRegClassifier.h>
 #import <LearnKit/LNKOptimizationAlgorithm.h>
+#import <LearnKit/LNKPredictor.h>
 #import <LearnKit/LNKUtilities.h>
