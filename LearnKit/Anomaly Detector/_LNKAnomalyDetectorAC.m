178285271919d3dedb7f5498a478a8cb812868d0
Matt Rajca <matt.rajca@me.com>
2014-10-10 20:21:58 -0500

    Anomaly detector: create the Sigma covariance matrix

--- LearnKit/Anomaly Detector/_LNKAnomalyDetectorAC.m
+++ LearnKit/Anomaly Detector/_LNKAnomalyDetectorAC.m
@@ -22,10 +22,11 @@
 	const LNKFloat *matrixBuffer = matrix.matrixBuffer;
 	
 	_mu = LNKFloatAlloc(columnCount);
-	_sigma2 = LNKFloatAlloc(columnCount);
+	_sigma2 = LNKFloatCalloc(columnCount * columnCount);
 	
 	LNKFloat *workgroup = LNKFloatAlloc(exampleCount);
 	
+	// Compute Gaussian parameters.
 	for (LNKSize column = 0; column < columnCount; column++) {
 		const LNKFloat *columnPointer = matrixBuffer + column;
 		
@@ -33,7 +34,9 @@
 		LNK_vmean(columnPointer, columnCount, &mean, exampleCount);
 		
 		_mu[column] = mean;
-		_sigma2[column] = LNK_pow(LNK_vsd(columnPointer, exampleCount, columnCount, workgroup, mean, NO), 2);
+		
+		// `sigma2` is actually a (diagonal) covariance matrix.
+		_sigma2[column * columnCount + column] = LNK_pow(LNK_vsd(columnPointer, exampleCount, columnCount, workgroup, mean, NO), 2);
 	}
 	
 	free(workgroup);
