2a17b33db2b21f2bf3feb4b9cc6fb1fc6544df96
Matt Rajca <matt.rajca@me.com>
2014-11-06 13:08:05 -0600

    Added note on indicator and output matrices

--- LearnKit/Collaborative Filtering/LNKCollaborativeFilteringPredictor.h
+++ LearnKit/Collaborative Filtering/LNKCollaborativeFilteringPredictor.h
@@ -13,6 +13,7 @@
 - (instancetype)initWithMatrix:(LNKMatrix *)matrix implementationType:(LNKImplementationType)implementationType optimizationAlgorithm:(id<LNKOptimizationAlgorithm>)algorithm userCount:(NSUInteger)userCount;
 
 /// The indicator and output matrices must be of `exampleCount` * `userCount` size.
+/// Both the indicator and output matrices must be set prior to training the predictor.
 - (void)copyIndicatorMatrix:(const LNKFloat *)matrix shouldTranspose:(BOOL)shouldTranspose;
 - (void)copyOutputMatrix:(const LNKFloat *)matrix shouldTranspose:(BOOL)shouldTranspose;
 
