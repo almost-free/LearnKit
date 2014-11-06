b1b555db351d63194314ac4e8f7f8527f4271a5f
Matt Rajca <matt.rajca@me.com>
2014-11-06 13:15:10 -0600

    Updated comments

--- LearnKit/Collaborative Filtering/LNKCollaborativeFilteringPredictor.h
+++ LearnKit/Collaborative Filtering/LNKCollaborativeFilteringPredictor.h
@@ -8,14 +8,17 @@
 #import "LNKPredictor.h"
 
 /// A collaborative filtering predictor for recommendation engines.
-/// Only the Conjugate Gradient optimization algorithm is supported.
+/// Supported optimization algorithms:
+///  - Conjugate Gradient/Accelerate
 @interface LNKCollaborativeFilteringPredictor : LNKPredictor
 
+/// Initializes a new collaborative filtering predictor.
 /// The `userCount` must be greater than 0.
 - (instancetype)initWithMatrix:(LNKMatrix *)matrix implementationType:(LNKImplementationType)implementationType optimizationAlgorithm:(id<LNKOptimizationAlgorithm>)algorithm userCount:(NSUInteger)userCount;
 
-/// The indicator and output matrices must be of `exampleCount` * `userCount` size.
 /// Both the indicator and output matrices must be set prior to training the predictor.
+/// The indicator and output matrices must be of `exampleCount` * `userCount` size.
+/// A copy of the input matrix will be made.
 - (void)copyIndicatorMatrix:(const LNKFloat *)matrix shouldTranspose:(BOOL)shouldTranspose;
 - (void)copyOutputMatrix:(const LNKFloat *)matrix shouldTranspose:(BOOL)shouldTranspose;
 
