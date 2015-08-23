32168e253160925f308d9c407d4dc0e4c2c7014b
Matt Rajca <matt.rajca@me.com>
2015-08-23 17:30:03 -0500

    Updated Naive Bayes test

--- LearnKitTests/NaiveBayesTests.m
+++ LearnKitTests/NaiveBayesTests.m
@@ -17,27 +17,33 @@
 
 @implementation NaiveBayesTests
 
-- (void)test1 {
+- (void)runTestWithSumOfLogarithmsComputations:(BOOL)computesSumOfLogarithms {
 	// Columns of Flu.csv: chills, runny nose, headache, fever, flu? (output)
 	NSString *path = [[NSBundle bundleForClass:[self class]] pathForResource:@"Flu" ofType:@"csv"];
 	LNKMatrix *matrix = [[LNKMatrix alloc] initWithCSVFileAtURL:[NSURL fileURLWithPath:path] addingOnesColumn:NO];
-	
+
 	LNKNaiveBayesClassifier *classifier = [[LNKNaiveBayesClassifier alloc] initWithMatrix:matrix implementationType:LNKImplementationTypeAccelerate optimizationAlgorithm:nil classes:[LNKClasses withCount:2]];
+	classifier.computesSumOfLogarithms = computesSumOfLogarithms;
 	[matrix release];
-	
+
 	[classifier registerValues:@[ @0, @1 ] forColumn:0];
 	[classifier registerValues:@[ @0, @1 ] forColumn:1];
 	[classifier registerValues:@[ @0, @1, @2 ] forColumn:2];
 	[classifier registerValues:@[ @0, @1 ] forColumn:3];
-	
+
 	[classifier train];
-	
+
 	const LNKFloat inputVector[] = {1,0,1,0};
 	LNKClass *outputClass = [classifier predictValueForFeatureVector:LNKVectorMakeUnsafe(inputVector, 4)];
-	
+
 	XCTAssertEqual(outputClass.unsignedIntegerValue, 0ULL, @"We should not have the flu");
-	
+
 	[classifier release];
+}
+
+- (void)testNaiveBayes {
+	[self runTestWithSumOfLogarithmsComputations:YES];
+	[self runTestWithSumOfLogarithmsComputations:NO];
 }
 
 @end
