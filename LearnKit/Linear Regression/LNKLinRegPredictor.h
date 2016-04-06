c3c79794260cc5a0b85e4fd2d322be26d081d985
Matt Rajca <matt.rajca@me.com>
2016-04-06 12:49:08 -0500

    LinRegPredictor: Added nullability additions

--- LearnKit/Linear Regression/LNKLinRegPredictor.h
+++ LearnKit/Linear Regression/LNKLinRegPredictor.h
@@ -7,9 +7,12 @@
 
 #import "LNKPredictor.h"
 
+NS_ASSUME_NONNULL_BEGIN
+
 /// For linear regression predictors, the gradient descent, normal equations, and L-BFGS algorithms are supported.
 /// Regularization is only supported for gradient descent and L-BFGS algorithms.
 /// Predicted values are of type NSNumber / LNKFloat.
 @interface LNKLinRegPredictor : LNKPredictor
-
 @end
+
+NS_ASSUME_NONNULL_END
