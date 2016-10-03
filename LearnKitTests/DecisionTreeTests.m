592decd4f35f1693af090e155f8e27bcb9cffaab
Matt Rajca <matt.rajca@me.com>
2016-10-03 09:25:46 -0500

    Added a new decision tree test

--- LearnKitTests/DecisionTreeTests.m
+++ LearnKitTests/DecisionTreeTests.m
@@ -54,4 +54,35 @@
 	[classifier release];
 }
 
+- (void)testHiring {
+	NSBundle *bundle = [NSBundle bundleForClass:self.class];
+	NSURL *matrixURL = [bundle URLForResource:@"Hiring" withExtension:@"csv"];
+	LNKMatrix *matrix = [[LNKMatrix alloc] initWithCSVFileAtURL:matrixURL];
+	XCTAssertNotNil(matrix);
+
+	LNKDecisionTreeClassifier *classifier = [[LNKDecisionTreeClassifier alloc] initWithMatrix:matrix
+																		   implementationType:LNKImplementationTypeAccelerate
+																		optimizationAlgorithm:nil
+																					  classes:[LNKClasses withCount:2]]; /* binary */
+	[matrix release];
+
+	[classifier registerCategoricalValues:3 forColumnAtIndex:0];
+	[classifier registerCategoricalValues:3 forColumnAtIndex:1];
+	[classifier registerBooleanValueForColumnAtIndex:2];
+	[classifier registerBooleanValueForColumnAtIndex:3];
+
+	[classifier validate];
+	[classifier train];
+
+	const LNKSize newExampleLength = 4;
+	LNKFloat newExample1[newExampleLength] = { 0, 0, 1, 0 };
+	LNKFloat newExample2[newExampleLength] = { 0, 0, 1, 1 };
+	LNKClass *class1 = [classifier predictValueForFeatureVector:LNKVectorCreateUnsafe(newExample1, newExampleLength)];
+	XCTAssertEqual(class1.unsignedIntegerValue, 1UL);
+	LNKClass *class2 = [classifier predictValueForFeatureVector:LNKVectorCreateUnsafe(newExample2, newExampleLength)];
+	XCTAssertEqual(class2.unsignedIntegerValue, 0UL);
+
+	[classifier release];
+}
+
 @end
